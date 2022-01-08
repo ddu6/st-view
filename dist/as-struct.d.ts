518020564e737ea98966f043bce9382a7810a54c
DDU <ddu6@protonmail.com>
2022-01-08 15:32:51 +0800

    update

--- dist/as-struct.d.ts
+++
@@ -1,9 +0,0 @@
-export declare function createNamedElement(name: string, element: Element): HTMLDivElement;
-export declare function createASStruct(): {
-    element: HTMLDivElement;
-    main: HTMLElement;
-    sideContent: HTMLDivElement;
-    article: HTMLElement;
-    panel: HTMLDivElement;
-    settings: HTMLDetailsElement;
-};
