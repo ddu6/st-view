94b2d489d16ea3da12e86acde483cec4f6135311
DDU <ddu6@protonmail.com>
2022-01-13 02:31:31 +0800

    update

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ import {init} from 'https://cdn.jsdelivr.net/gh/st-org/stui@0.11.0/mod.js'
 import {createViewer} from './mod.js'
 init()
 const style = document.createElement('style')
-style.textContent = `@import url(https://cdn.jsdelivr.net/gh/st-org/st-std@0.23.0/mod.js);`
+style.textContent = `@import url("./main.css");`
 document.head.append(style)
 const viewer = window.viewer = await createViewer()
 document.head.append(viewer.style)
