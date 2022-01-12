b77be10cde1d67f47be5ffe62229961128ec4b8e
DDU <ddu6@protonmail.com>
2022-01-13 02:41:54 +0800

    update

--- main.js
+++ main.js
@@ -1,9 +1,6 @@
-import {init} from 'https://cdn.jsdelivr.net/gh/st-org/stui@0.11.0/mod.js'
+import {init} from 'https://cdn.jsdelivr.net/gh/st-org/stui@0.11.1/mod.js'
 import {createViewer} from './mod.js'
 init()
-const style = document.createElement('style')
-style.textContent = `@import url("./main.css");`
-document.head.append(style)
 const viewer = window.viewer = await createViewer()
 document.head.append(viewer.style)
 document.body.append(viewer.element)
--- src/import.ts
+++ src/import.ts
@@ -8,6 +8,6 @@ export const {getMod} = createImporter<{
     ucs: typeof tagToUnitCompiler
 }>({
     stc: 'https://cdn.jsdelivr.net/gh/st-org/stc@0.19.1/mod.js',
-    stui: 'https://cdn.jsdelivr.net/gh/st-org/stui@0.11.0/mod.js',
-    ucs: 'https://cdn.jsdelivr.net/gh/st-org/sthl@0.23.1/ucs.js'
+    stui: 'https://cdn.jsdelivr.net/gh/st-org/stui@0.11.1/mod.js',
+    ucs: 'https://cdn.jsdelivr.net/gh/st-org/st-std@0.23.3/ucs.js'
 })
