ed0b000c99f0b8086641fd6778b1c4ccd06b3897
DDU <ddu6@protonmail.com>
2022-01-19 19:43:58 +0800

    update

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import {init} from 'https://cdn.jsdelivr.net/gh/st-org/stui@0.15.3/mod.js'
+import {init} from 'https://cdn.jsdelivr.net/gh/st-org/stui@0.15.4/mod.js'
 import {createViewer} from './mod.js'
 init()
 const viewer = window.viewer = await createViewer()
