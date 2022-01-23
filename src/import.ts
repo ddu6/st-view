fa605ededa0838c47921081462458204c2ca67dc
DDU <ddu6@protonmail.com>
2022-01-23 22:03:52 +0800

    update

--- src/import.ts
+++ src/import.ts
@@ -9,5 +9,5 @@ export const {getMod} = createImporter<{
 }>({
     stc: 'https://cdn.jsdelivr.net/gh/st-org/stc@0.19.3/mod.js',
     stui: 'https://cdn.jsdelivr.net/gh/st-org/stui@0.15.4/mod.js',
-    ucs: 'https://cdn.jsdelivr.net/gh/st-org/st-std@0.26.0/ucs.js'
+    ucs: 'https://cdn.jsdelivr.net/gh/st-org/st-std@0.26.1/ucs.js'
 })
