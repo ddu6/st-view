588ba59e2c530fc0f2347935e21429038a79d6af
DDU <ddu6@protonmail.com>
2022-03-21 02:10:18 +0800

    update

--- main.js
+++ main.js
@@ -3,8 +3,10 @@ import {createViewer} from './mod.js'
 init()
 const style = document.createElement('style')
 document.head.append(style)
-style.textContent = `@import ${JSON.stringify(new URL('./main.css', import.meta.url).href)};`
-const viewer = window.viewer = await createViewer()
-document.head.append(viewer.style)
-document.body.append(viewer.element)
-await viewer.autoLoad()
+style.textContent = `@import ${JSON.stringify(new URL('./main.css', import.meta.url).href)};`;
+(async () => {
+    const viewer = window.viewer = await createViewer()
+    document.head.append(viewer.style)
+    document.body.append(viewer.element)
+    await viewer.autoLoad()
+})()
