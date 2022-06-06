da83b0ad8c5b7ca76d31a8567261b2dfe001b402
DDU <ddu6@protonmail.com>
2022-06-06 23:01:58 +0800

    update

--- dist/import.js
+++ dist/import.js
@@ -1,10 +1,5 @@
 import { createImporter } from '@ddu6/importer';
 export const { getMod } = createImporter({
-    /*
-        stc: 'https://cdn.jsdelivr.net/gh/st-org/stc@0.28.3/mod.js',
-        stui: 'https://cdn.jsdelivr.net/gh/st-org/stui@0.15.12/mod.js',
-        ucs: 'https://cdn.jsdelivr.net/gh/st-org/st-std@0.31.4/ucs.js'
-    */
     stc: '../stc@0.28.3/mod.js',
     stui: '../stui@0.15.12/mod.js',
     ucs: '../st-std@0.31.4/ucs.js'
--- src/import.ts
+++ src/import.ts
@@ -7,11 +7,6 @@ export const {getMod} = createImporter<{
     stui: typeof stui
     ucs: typeof tagToUnitCompiler
 }>({
-/*
-    stc: 'https://cdn.jsdelivr.net/gh/st-org/stc@0.28.3/mod.js',
-    stui: 'https://cdn.jsdelivr.net/gh/st-org/stui@0.15.12/mod.js',
-    ucs: 'https://cdn.jsdelivr.net/gh/st-org/st-std@0.31.4/ucs.js'
-*/
     stc: '../stc@0.28.3/mod.js',
     stui: '../stui@0.15.12/mod.js',
     ucs: '../st-std@0.31.4/ucs.js'
