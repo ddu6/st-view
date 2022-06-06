772bd03bd17c5cabb93ec31a587199f2f6d4c9b9
DDU <ddu6@protonmail.com>
2022-06-06 11:37:38 +0800

    update

--- src/import.ts
+++ src/import.ts
@@ -8,11 +8,11 @@ export const {getMod} = createImporter<{
     ucs: typeof tagToUnitCompiler
 }>({
 /*
-    stc: 'https://cdn.jsdelivr.net/gh/st-org/stc@0.28.2/mod.js',
+    stc: 'https://cdn.jsdelivr.net/gh/st-org/stc@0.28.3/mod.js',
     stui: 'https://cdn.jsdelivr.net/gh/st-org/stui@0.15.12/mod.js',
     ucs: 'https://cdn.jsdelivr.net/gh/st-org/st-std@0.31.4/ucs.js'
 */
-    stc: '../stc@0.28.2/mod.js',
+    stc: '../stc@0.28.3/mod.js',
     stui: '../stui@0.15.12/mod.js',
     ucs: '../st-std@0.31.4/ucs.js'
 })
