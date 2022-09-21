71346b4009583d8475ade446823bce711e3a99bf
DDU <ddu6@protonmail.com>
2022-09-21 20:04:38 +0800

    update

--- src/viewer.ts
+++ src/viewer.ts
@@ -104,6 +104,9 @@ export async function createViewer() {
         }
     }
     function loadCompileResult({compiler, documentFragment}: Awaited<ReturnType<typeof compile>>) {
+        if (compiler.stop) {
+            return false
+        }
         if (compiler.context.title.length > 0) {
             document.title = compiler.context.title
         }
@@ -112,20 +115,22 @@ export async function createViewer() {
         nav.innerHTML = ''
         nav.append(headingTreeToElement(extractHeadingTree(compiler.context, compiler.base.unitToInlinePlainString)))
         env.compiler = compiler
+        return true
     }
     async function load(urls: string[]) {
         element.classList.add('loading')
         const {compileURLs} = await getMod('stc')
-        loadCompileResult(await compileURLs(urls, {
+        const result = loadCompileResult(await compileURLs(urls, {
             builtInTagToUnitCompiler: await getMod('ucs'),
             style
         }))
         element.classList.remove('loading')
+        return result
     }
     async function loadString(string: string, url: string) {
         element.classList.add('loading')
         const {compile} = await getMod('stc')
-        loadCompileResult(await compile([{
+        const result = loadCompileResult(await compile([{
             value: string,
             url
         }], {
@@ -133,6 +138,7 @@ export async function createViewer() {
             style
         }))
         element.classList.remove('loading')
+        return result
     }
     async function autoLoad() {
         const {dataset} = document.documentElement
@@ -166,9 +172,13 @@ export async function createViewer() {
             focusId = dataset.focusId
         )
         if (string !== undefined) {
-            await loadString(string, dir)
+            if (!await loadString(string, dir)) {
+                return
+            }
         } else if (src !== undefined) {
-            await load([src])
+            if (!await load([src])) {
+                return
+            }
         }
         await focus(dir, focusURL, focusPositionStr, focusId)
     }
