564acdb5cf87e69d413d6b61f7d870a431006f50
DDU <ddu6@protonmail.com>
2022-11-22 10:10:00 +0800

    update

--- dist/import.js
+++ dist/import.js
@@ -1,6 +1,6 @@
 import { createImporter } from '@ddu6/importer';
 export const { getMod } = createImporter({
-    stc: '../stc@0.28.4/mod.js',
+    stc: '../stc@0.28.5/mod.js',
     stui: '../stui@0.15.12/mod.js',
     ucs: '../st-std@0.31.5/ucs.js'
 });
