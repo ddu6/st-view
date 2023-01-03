c0c9a1c3b9e0020a37e1da4ebad4f9398cd343bc
DDU <ddu6@protonmail.com>
2023-01-03 18:05:16 +0800

    update

--- dist/import.js
+++ dist/import.js
@@ -2,5 +2,5 @@ import { createImporter } from '@ddu6/importer';
 export const { getMod } = createImporter({
     stc: '../stc@0.28.6/mod.js',
     stui: '../stui@0.31.8/mod.js',
-    ucs: '../st-std@0.31.9/ucs.js'
+    ucs: '../st-std@0.31.10/ucs.js'
 });
--- src/import.ts
+++ src/import.ts
@@ -9,5 +9,5 @@ export const {getMod} = createImporter<{
 }>({
     stc: '../stc@0.28.6/mod.js',
     stui: '../stui@0.31.8/mod.js',
-    ucs: '../st-std@0.31.9/ucs.js'
+    ucs: '../st-std@0.31.10/ucs.js'
 })
